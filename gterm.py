b710eed47658561f133ee80ae77f41820c884f38
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-09-11 21:17:10 -0500

    Enhance search functionality with data transformation and interactive pagination
    - Add support for displaying raw JSON results using the new `paginate()` function.
    - Implement transformation of search results into `Game` model objects via the `data.transform()` function.
    - Update search to prepare for interactive result navigation, integrating with the `Game` model for better data handling.
    - Improve modularity and readability by using the `Game` model defined in `models.py` for structured data representation.

--- gterm.py
+++ gterm.py
@@ -1,9 +1,9 @@
 #!.venv/bin/python
 import typer
-import data
 from rich.console import Console as rConsole
 from rich import print
 from typing_extensions import Annotated
+import data
 
 
 app = typer.Typer()
@@ -49,12 +49,22 @@ def search(
         return
 
     if raw_json:
-        console = rConsole()
-        with console.pager():
-            console.print(data.search_rawg_api(keyword).json())
-        return
+        paginate(data.query_rawg_api(keyword).json())
 
-    print(data.search_rawg_api(keyword).json())
+    search_results = data.query_rawg_api(keyword).json()
+    print(data.transform(search_results))
+    # replace print above with interactive interface
+    # navigate search results, select game for more info, or request next page
+
+
+def paginate(text):
+    """
+    Displays TEXT via terminal pager
+    """
+    console = rConsole()
+    with console.pager():
+        console.print(text)
+    return
 
 
 if __name__ == "__main__":
