ae555cfe1c15e502fcbe6305c28a28bd200daf30
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-09-11 21:14:18 -0500

    Refactor RAWG API query and introduce data transformation with Game model
    - Transition from `search_rawg_api()` to `query_rawg_api()` to enhance code clarity and structure.
    - Add `query_rawg_api()` function to support flexible querying of the RAWG API by keyword and endpoint.
    - Add `transform()` function to convert raw JSON search results into a list of `Game` model objects from `models.py`.
    - Add `is_cached()` function to check if a response is cached.
    - Import the `Game` Pydantic model to ensure structured data handling from the API.

--- data.py
+++ data.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from datetime import timedelta
 import requests
 import requests_cache
+import models
 
 load_dotenv()
 RAWG_KEY = os.getenv("RAWG_API_KEY")
@@ -16,12 +17,29 @@ base_params = {
 }
 
 
-def search_rawg_api(search: str):
-    new_params = {"search": f"{search}"}
+def query_rawg_api(keyword: str, endpoint: str = "/games"):
+    """Queries KEYWORD from RAWG API"""
+    new_params = {"search": f"{keyword}"}
     new_params.update(base_params)
-    response = rawg_session.get(RAWG_URL + "/games", params=new_params)
+    response = rawg_session.get(RAWG_URL + endpoint, params=new_params)
     return response
 
 
 def clear_cache():
+    """clear cached api responses"""
     rawg_session.cache.clear()
+
+
+def is_cached(response) -> bool:
+    """return BOOL on response being cached or not"""
+    cache_obj = requests_cache.models.response.CachedResponse
+    return isinstance(response, cache_obj)
+
+
+def transform(game_search_results: dict) -> list[models.Game]:
+    """convert raw json results to list of Game() objects"""
+    transformed_results: list = []
+    games: list = game_search_results.get("results")
+    for game in games:
+        transformed_results.append(models.Game.construct(**game))
+    return transformed_results
