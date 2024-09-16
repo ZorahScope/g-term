0b31863d5d458395965d9e7cad2ed745444b8aec
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-09-15 22:15:24 -0500

    Move search command to separate file

---
+++ search.py
@@ -0,0 +1,55 @@
+import typer
+from rich.console import Console as rConsole
+from rich import print
+from typing_extensions import Annotated
+import data
+
+app = typer.Typer()
+
+
+@app.command()
+def game(
+    keyword: Annotated[
+        str, typer.Argument(help="The name of the game your searching for")
+    ] = "",
+    clear_cache: Annotated[
+        bool, typer.Option(help="Deletes cached search results")
+    ] = False,
+    raw_json: Annotated[
+        bool, typer.Option(help="Displays raw json search results in terminal pager")
+    ] = False,
+) -> None:
+    """
+    Displays a list of games relevant to KEYWORD
+    """
+
+    if not keyword:
+        keyword = typer.prompt("Enter game your searching for")
+
+    if clear_cache:
+        data.clear_cache()
+        print("[bold red]Notice![/bold red] search cache has been cleared")
+        return
+
+    if raw_json:
+        paginate(data.query_rawg_api(keyword).json())
+
+    search_results = data.query_rawg_api(keyword).json()
+    print(data.transform(search_results))
+    # replace print above with interactive interface
+    # navigate search results, select game for more info, or request next page
+
+
+
+def filter():
+    pass
+
+
+def paginate(text):
+    """
+    Displays TEXT via terminal pager
+    """
+    console = rConsole()
+    with console.pager():
+        console.print(text)
+    return
