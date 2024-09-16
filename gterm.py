ad93dde682a2d0220465cf60e6997f747efed4bf
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-09-15 22:04:54 -0500

    Refactor search to separate file to make room for subcommands

--- gterm.py
+++ gterm.py
@@ -3,10 +3,15 @@ import typer
 from rich.console import Console as rConsole
 from rich import print
 from typing_extensions import Annotated
+import search
+import ls
 import data
 
 
+
 app = typer.Typer()
+app.add_typer(search.app, name="search")
+app.add_typer(ls.app, name="list")
 
 
 @app.command()
@@ -22,49 +27,6 @@ def price():
 @app.command()
 def stats():
     pass
-
-
-@app.command()
-def search(
-    keyword: Annotated[
-        str, typer.Argument(help="The name of the game your searching for")
-    ] = "",
-    clear_cache: Annotated[
-        bool, typer.Option(help="Deletes cached search results")
-    ] = False,
-    raw_json: Annotated[
-        bool, typer.Option(help="Displays raw json search results in terminal pager")
-    ] = False,
-) -> None:
-    """
-    Displays a list of games relevant to KEYWORD
-    """
-
-    if not keyword:
-        keyword = typer.prompt("Enter game your searching for")
-
-    if clear_cache:
-        data.clear_cache()
-        print("[bold red]Notice![/bold red] search cache has been cleared")
-        return
-
-    if raw_json:
-        paginate(data.query_rawg_api(keyword).json())
-
-    search_results = data.query_rawg_api(keyword).json()
-    print(data.transform(search_results))
-    # replace print above with interactive interface
-    # navigate search results, select game for more info, or request next page
-
-
-def paginate(text):
-    """
-    Displays TEXT via terminal pager
-    """
-    console = rConsole()
-    with console.pager():
-        console.print(text)
-    return
 
 
 if __name__ == "__main__":
